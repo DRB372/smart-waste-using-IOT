108f3cafcb2800d7dcdbfb93358c9cf7e9600c6b
fmunirWorld <fmunir923@gmail.com>
2020-02-25 10:32:11 +0500

    add dotenv to use environment variables

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const mysql = require('mysql');
 const createError = require('http-errors');
 
 const auth = require('./lib/auth');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'swc_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect(err => {
